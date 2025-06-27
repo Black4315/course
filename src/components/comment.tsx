c084f9c823c97cda3ca2f7205c0bd61382de96df
Black4315 <yurytube4315@gmail.com>
2025-06-27 19:35:25 +0300

    comment

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -3,16 +3,16 @@ import { useState } from "react";
 export default function Comment({ comment }: { comment: string; }) {
     const [expanded, setExpanded] = useState(false);
     let isText = typeof comment === 'string';
-    const isLong = isText? comment.length > 150 : false;
-
+    const isLong = isText? comment.length > 175 : false;
+    console.log(comment.length)
     return (
         <div className="text-[#999] text-[1.1rem] sm:text-lg mt-3 whitespace-pre-wrap">
             <p
-                className={`break-words ${!expanded ? "line-clamp-3" : ""}`}
+                className={`break-words ${!expanded && isLong ? "line-clamp-2" : ""}`}
                 style={{
                     display: "-webkit-box",
                     WebkitBoxOrient: "vertical",
-                    WebkitLineClamp: !expanded ? 3 : "unset",
+                    WebkitLineClamp: !expanded && isLong ? 2 : "unset",
                     overflow: "hidden",
                 }}
             >
